96314c8fc3c74630d1207133f4c22567e8d42ab5
jatin-wipro <66778854+jatin-wipro@users.noreply.github.com>
2021-07-06 15:00:00 +0530

    Add files via upload

---
+++ learn-terraform-data-sources-app/main.tf
@@ -0,0 +1,91 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+data "terraform_remote_state" "vpc" {
+  backend = "local"
+
+  config = {
+    path = "../learn-terraform-data-sources-vpc/terraform.tfstate"
+  }
+}
+
+provider "aws" {
+#  region = "us-east-1"
+   region = data.terraform_remote_state.vpc.outputs.aws_region
+   access_key = "AKIAVBR3OMXTAZLFOZPQ"
+   secret_key = "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"
+
+}
+
+resource "random_string" "lb_id" {
+  length  = 3
+  special = false
+}
+
+module "elb_http" {
+  source  = "terraform-aws-modules/elb/aws"
+  version = "2.4.0"
+
+  # Ensure load balancer name is unique
+  name = "lb-${random_string.lb_id.result}-tutorial-example"
+
+  internal = false
+
+#  security_groups = []
+#  subnets         = []
+  security_groups = data.terraform_remote_state.vpc.outputs.lb_security_group_ids
+  subnets         = data.terraform_remote_state.vpc.outputs.public_subnet_ids
+  number_of_instances = length(aws_instance.app)
+  instances           = aws_instance.app.*.id
+
+  listener = [{
+    instance_port     = "80"
+    instance_protocol = "HTTP"
+    lb_port           = "80"
+    lb_protocol       = "HTTP"
+  }]
+
+  health_check = {
+    target              = "HTTP:80/index.html"
+    interval            = 10
+    healthy_threshold   = 3
+    unhealthy_threshold = 10
+    timeout             = 5
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "app" {
+  count = var.instances_per_subnet * length(data.terraform_remote_state.vpc.outputs.private_subnet_ids)
+#  ami = "ami-0277b52859bac6f4b"
+  ami = data.aws_ami.amazon_linux.id
+
+  instance_type = var.instance_type
+
+#  subnet_id              = ""
+#  vpc_security_group_ids = []
+   subnet_id              = data.terraform_remote_state.vpc.outputs.private_subnet_ids[count.index % length(data.terraform_remote_state.vpc.outputs.private_subnet_ids)]
+   vpc_security_group_ids = data.terraform_remote_state.vpc.outputs.app_security_group_ids
+
+user_data = <<-EOF
+    #!/bin/bash
+    sudo yum update -y
+    sudo yum install httpd -y
+    sudo systemctl enable httpd
+    sudo systemctl start httpd
+    echo "<html><body><div>Hello, world!</div></body></html>" > /var/www/html/index.html
+    EOF
+}
